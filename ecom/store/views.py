f9b523af2e7c6ef66b748a6f9a08811a2fbd1060
HesamNajafi <hesamnajafi39@gmail.com>
2023-08-28 12:30:48 +0430

    my changes

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 from .models import *
 
 # Create your views here.
@@ -34,4 +35,37 @@ def checkout(request):
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
-    return JsonResponse('Item was added', safe=False)
+    # Load JSON data from the request body
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    # Print action and productId for debugging
+    print('Action:', action)
+    print('productId:', productId)
+
+    # Get the customer and product objects
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+
+    # Get or create an order for the customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    # Get or create the order item for the product in the current order
+    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    # Update the quantity based on the action
+    if action == 'add':
+        order_item.quantity += 1
+    elif action == 'remove':
+        order_item.quantity -= 1
+
+    # Save the order item
+    order_item.save()
+
+    # If the quantity becomes <= 0, delete the order item
+    if order_item.quantity <= 0:
+        order_item.delete()
+
+    # Return a response indicating success
+    return JsonResponse('Item was updated', safe=False)
